174f2041c2c2ec2f987c1d70fd466ba9c6508dc5
Divakar <bdivakar0786@gmail.com>
2026-01-16 10:26:39 +0530

    Day 5 file

---
+++ Day 5/ruby.rb
@@ -0,0 +1,20 @@
+# puts "Single line comments below"
+
+# Ruby Single-Line Comments
+
+# puts "Multi line comments below"
+
+=begin
+Comment line 1 
+Comment line 2  
+Comment line 3
+=end
+
+
+
+
+
+
+
+
+
