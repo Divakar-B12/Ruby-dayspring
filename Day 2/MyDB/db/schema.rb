da4bd96d60fd4344c76541c88a1aacd850fcddfb
Divakar <bdivakar0786@gmail.com>
2026-02-01 21:55:57 +0530

    created new order table

--- Day 2/MyDB/db/schema.rb
+++ Day 2/MyDB/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_30_064054) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_01_161912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -57,6 +57,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_30_064054) do
     t.string "email"
     t.string "name"
     t.integer "phone_number"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "count"
+    t.datetime "created_at", null: false
+    t.string "details"
     t.datetime "updated_at", null: false
   end
 
